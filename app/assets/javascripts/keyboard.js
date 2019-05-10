68dccf546c79e982d7f6940ed8d6339febe66e4d
AntoniaSaez <asaez1@miuandes.cl>
2019-05-09 23:25:24 -0400

    load keyboard.js dynamically

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,7 +1,18 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
 	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+});
+
+$(() => {
+	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
 	$('.key').click(function(){
 		console.log($(this).text())
 	});
+});
+var key = "";
+$(document).ready(function(){
+    $("div.key").click(function(){
+        key += $(this).text()
+        $("#tcontainer").text(key);
+    });
 });
